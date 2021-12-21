455580f3417c255571c0f34cb32359b16776b9d2
Nelson Rivera <nyrivera@student.rtc.edu>
2021-12-21 03:46:47 -0800

    Bubble Array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int[] intArray ={20, 35, -15, 7, 55, 1, -22};
+
+        for(int lastUnsortedIndex = intArray.length -1;lastUnsortedIndex>0;lastUnsortedIndex -- ){
+            for (int i = 0; i<lastUnsortedIndex;i++){
+                if (intArray[i] > intArray[i+1]){
+                    swap(intArray, i, i+1);
+                };
+            }
+        }
+        for (int i=0;i< intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
 
     }
 
@@ -15,6 +27,7 @@ public class Main {
         }
         int temp = array[i];
         array[i] = array[j];
+        array[j]= temp;
     }
 
 }
