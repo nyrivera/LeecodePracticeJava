e5c1c0d8a59876d34b755d9baa7d281482c196ef
Nelson Rivera <nyrivera@student.rtc.edu>
2021-12-21 03:11:44 -0800

    Bubble Array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,4 +7,14 @@ public class Main {
 
     }
 
+    // swap method
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+    }
+
 }
